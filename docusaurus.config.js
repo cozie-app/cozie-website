65be8a048ad4f8035e8a2b9f7817bd26b7d74da6
Mario Frei <mario.frei@gmx.net>
2022-12-10 21:55:29 +0800

    change "edit this page" button link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,14 +40,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/cozie-app/cozie-website/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/cozie-app/cozie-website/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
